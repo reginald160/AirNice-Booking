e0c51ef7a9dfa68d432ece821e5ed6adf41db676
reginald160 <ozougwuifeanyi160@gmai.com>
2021-04-21 08:55:31 +0100

    twilio2

--- AirNice.Models/Helper/Attribute.cs
+++ AirNice.Models/Helper/Attribute.cs
@@ -37,5 +37,27 @@ namespace AirNice.Models.Helper
         }
     }
 
+    public class PhoneNumber : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value?.ToString()[0] != value?.ToString().ToUpper()[0])
+            {
+                return new ValidationResult("Value must start with capital letter");
+            }
+            if (value.ToString().Length > 20)
+            {
+                return new ValidationResult("Value must not be more more than 15 letters");
+            }
+            //if (_IsRequired.Equals(true) && value.ToString().Equals(String.Empty))
+            //{
+
+            //    return new ValidationResult($" The {validationContext.DisplayName} field is required");
+            //}
+
+
+            return ValidationResult.Success;
+        }
+    }
 
 }
